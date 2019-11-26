ae98f3dac2fd13f461992d18c98086a9258e0b22
neoli <=798060965@qq.com>
2019-11-26 12:24:38 +0800

    git 配置脚本

---
+++ git_config.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+git config --global user.name neoli
+git config --global user.email 798060965@qq.com
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.br branch
+git config --global alias.st status
